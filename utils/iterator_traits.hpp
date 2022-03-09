26cff75e115caef06fb9c5c00212bebe52e4a079
izenynn <me@izenynn.com>
2022-03-09 01:09:47 +0100

    feat: iterator_traits

---
+++ utils/iterator_traits.hpp
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iterator_traits.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 00:22:59 by dpoveda-          #+#    #+#             */
+/*   Updated: 2022/03/09 01:05:53 by dpoveda-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITERATOR_TRAITS_HPP_
+# define ITERATOR_TRAITS_HPP_
+
+# include <iterator>
+
+namespace ft {
+	template<class Iter>
+	struct iterator_traits {
+		typedef typename Iter::value_type			value_type;
+		typedef typename Iter::difference_type		difference_type;
+		typedef typename Iter::reference			reference;
+		typedef typename Iter::pointer				pointer;
+		typedef typename Iter::iterator_category	iterator_category;
+	};
+
+	template<class T>
+	struct iterator_traits<T*> {
+		typedef T								value_type;
+		typedef ptrdiff_t						difference_type;
+		typedef T&								reference;
+		typedef T*								pointer;
+		typedef std::random_access_iterator_tag	iterator_category;
+	};
+
+	template<class T>
+	struct iterator_traits<const T*> {
+		typedef T								value_type;
+		typedef ptrdiff_t						difference_type;
+		typedef const T&						reference;
+		typedef const T*						pointer;
+		typedef std::random_access_iterator_tag	iterator_category;
+	};
+}
+
+#endif

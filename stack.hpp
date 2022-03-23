31c25ab8d99ecdebfe5fbad9efdc33e3259fc763
izenynn <me@izenynn.com>
2022-03-23 08:47:05 +0100

    wip: stack

---
+++ stack.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 09:59:50 by dpoveda-          #+#    #+#             */
+/*   Updated: 2022/03/23 08:46:43 by dpoveda-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STACK_HPP_
+# define STACK_HPP_
+
+# include "vector.hpp"
+
+namespace ft {
+	template<class T, class Container = ft::vector<T> >
+	class stack {
+	};
+}
+
+#endif

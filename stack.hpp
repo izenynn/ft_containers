09e4f079c95336348a704eafcd9481e641dd4adf
izenynn <me@izenynn.com>
2022-07-05 11:59:39 +0200

    feat: finish stack chore: add stack operators and stack functions

--- stack.hpp
+++ stack.hpp
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 09:59:50 by dpoveda-          #+#    #+#             */
-/*   Updated: 2022/03/23 09:03:20 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2022/07/05 11:50:02 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,89 @@ namespace ft {
 	template<class T, class Container = ft::vector<T> >
 	class stack {
 		public:
+			// definitions
 			typedef Container									container_type;
 			typedef typename container_type::value_type			value_type;
 			typedef typename container_type::size_type			size_type;
 			typedef typename container_type::reference			reference;
 			typedef typename container_type::const_reference	const_reference;
 
+			// constructors
+			explicit stack(const container_type& cntr = container_type())
+			: c(cntr) {}
+
+			// destructors
+			~stack(void) {}
+
+			// operator
+			stack& operator=(const stack& other) {
+				if (this != &other) {
+					this->c = other.c;
+				}
+				return *this;
+			}
+
+			// top
+			reference top(void) {
+				return this->c.back();
+			}
+			const_reference top(void) const {
+				return this->c.back();
+			}
+
+			// empty
+			bool empty(void) const {
+				return this->c.empty();
+			}
+
+			// size
+			size_type size(void) const {
+				return this->c.size();
+			}
+
+			// operations
+			void push(const value_type& value) {
+				this->c.push_back(value);
+			}
+			void pop(void) {
+				this->c.pop_back();
+			}
+
+			// operators
+			template< class T1, class C1 >
+			friend bool operator==(const stack<T1, C1> &lhs, const stack<T1, C1> &rhs);
+			template< class T1, class C1 >
+			friend bool operator>(const stack<T1, C1> &lhs, const stack<T1, C1> &rhs);
+
 		protected:
 			container_type c;
-		public:
-			explicit
-			stack(const container_type& cntr = container_type())
-			: c(cntr)
-			{}
-
-			~stack(void)
-			{}
-		private:
 	};
+
+	// operators
+	template< class T, class Container >
+	bool operator==( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs ) {
+		return (lhs.c == rhs.c);
+	}
+	template< class T, class Container >
+	bool operator!=( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs ) {
+		return !(lhs.c == rhs.c);
+	}
+	template< class T, class Container >
+	bool operator<( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs ) {
+		return (lhs.c < rhs.c);
+	}
+	template< class T, class Container >
+	bool operator<=( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs ) {
+		return !(rhs.c < lhs.c);
+	}
+	template< class T, class Container >
+	bool operator>( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs ) {
+		return (rhs.c < lhs.c);
+	}
+	template< class T, class Container >
+	bool operator>=( const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs ) {
+		return !(lhs.c < rhs.c);
+	}
 }
 
 #endif
---
+++ vector.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/05 08:56:18 by dpoveda-          #+#    #+#             */
+/*   Updated: 2022/07/05 08:58:04 by dpoveda-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP_
+# define VECTOR_HPP_
+
+# include <memory>
+
+namespace ft {
+	template<class T, class Allocator = std::allocator<T> >
+	class vector {
+	};
+}
+
+#endif

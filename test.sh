72f5a43c02264ef46851116a12553bd62433a8cb
rabilongo <rabi@LAPTOP-RABI.localdomain>
2022-07-11 18:14:50 +0200

    roses are red

--- test.sh
+++ test.sh
@@ -9,13 +9,11 @@ echo "=============================================================="
 echo "COMPILING:"
 
 #clang++ -std=c++98 -Wall -Wextra -Werror -Wshadow -pedantic -g3 -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=bounds -fsanitize=null main"$1".cpp
-g++ -std=c++98 -Wall -Wextra -Werror -Wshadow -pedantic -g3 -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=bounds -fsanitize=null main"$1".cpp
+#g++ -std=c++98 -Wall -Wextra -Werror -Wshadow -pedantic -g3 -fsanitize=address -fsanitize=leak -fsanitize=undefined -fsanitize=bounds -fsanitize=null main"$1".cpp
 
 #clang++ -std=c++98 -Wall -Wextra -Werror -Wshadow -pedantic -g3 -fsanitize=leak -fsanitize=undefined -fsanitize=bounds -fsanitize=null main"$1".cpp
 
-#clang++ -std=c++98 -Wall -Wextra -Werror -Wshadow -pedantic -g3 main"$1".cpp
-
-#clang++ -std=c++98 -g3 -fsanitize=address main"$1".cpp
+clang++ -std=c++98 -Wall -Wextra -Werror -Wshadow -pedantic -g3 main"$1".cpp
 
 #clang++ -std=c++98 -g3 main"$1".cpp
 

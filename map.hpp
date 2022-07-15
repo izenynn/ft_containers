95189aa4d4951757d51581c158242409adbaebdf
rabilongo <rabi@LAPTOP-RABI.localdomain>
2022-07-15 09:26:37 +0200

    electric dragons make me sick >.<

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: dpoveda- <me@izenynn.com>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 13:13:37 by dpoveda-          #+#    #+#             */
-/*   Updated: 2022/07/15 09:09:48 by dpoveda-         ###   ########.fr       */
+/*   Updated: 2022/07/15 09:26:31 by dpoveda-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,11 +44,11 @@ namespace ft {
 			typedef node_type&								node_reference;
 
 			typedef typename allocator_type::size_type			size_type;
-			typedef typename allocator_type::difference_type		difference_type;
+			typedef typename allocator_type::difference_type	difference_type;
 
 			typedef typename allocator_type::reference			reference;
-			typedef typename allocator_type::const_reference		const_reference;
-			typedef typename allocator_type::pointer				pointer;
+			typedef typename allocator_type::const_reference	const_reference;
+			typedef typename allocator_type::pointer			pointer;
 			typedef typename allocator_type::const_pointer		const_pointer;
 
 			class value_compare {
